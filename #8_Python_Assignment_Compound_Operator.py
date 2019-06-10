268c95745fc65395aa519c138ec55074c0275e32
to.patelvijay@gmail.com <to.patelvijay@gmail.com>
2019-06-10 10:40:07 +0530

    cosmetic changes

--- #8_Python_Assignment_Compound_Operator.py
+++ #8_Python_Assignment_Compound_Operator.py
@@ -23,23 +23,23 @@ print ("b value is ",b)
 print ("c value is ",c)
 
 c = a + b
-print ("Line 1 - Value of c is (c=a+b) ", c)
+print (" Value of c is (c=a+b) ", c)
 
 c += a
-print ("Line 2 - Value of c is (c +=a)", c)
+print (" Value of c is (c +=a)", c)
 
 c *= a
-print ("Line 3 - Value of c is (c *=a)", c)
+print (" Value of c is (c *=a)", c)
 
 c /= a
-print ("Line 4 - Value of c is (c /=a)", c)
+print (" Value of c is (c /=a)", c)
 
 c  = 2
 c %= a
-print ("Line 5 - Value of c is (c %=a)", c)
+print (" Value of c is (c %=a)", c)
 
 c **= a
-print ("Line 6 - Value of c is (c **= a)    ", c)
+print (" Value of c is (c **= a)    ", c)
 
 c //= a
-print ("Line 7 - Value of c is (c //=a)", c)
+print (" Value of c is (c //=a)", c)
