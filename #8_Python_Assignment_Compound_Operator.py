c5ba227e1285ae79ebe4c775eb5e4dfc6e10af44
to.patelvijay@gmail.com <to.patelvijay@gmail.com>
2019-06-10 10:34:44 +0530

    updated assignment operator script

--- #8_Python_Assignment_Compound_Operator.py
+++ #8_Python_Assignment_Compound_Operator.py
@@ -14,6 +14,32 @@
 # >>=	x >>= 5	x = x >> 5
 # <<=	x <<= 5	x = x << 5
 
-x=5
-#output=a+
-print ("a+ output is :",(x += 5),)
+a = 10
+b = 20
+c = 0
+
+print ("a value is ",a)
+print ("b value is ",b)
+print ("c value is ",c)
+
+c = a + b
+print ("Line 1 - Value of c is (c=a+b) ", c)
+
+c += a
+print ("Line 2 - Value of c is (c +=a)", c)
+
+c *= a
+print ("Line 3 - Value of c is (c *=a)", c)
+
+c /= a
+print ("Line 4 - Value of c is (c /=a)", c)
+
+c  = 2
+c %= a
+print ("Line 5 - Value of c is (c %=a)", c)
+
+c **= a
+print ("Line 6 - Value of c is (c **= a)    ", c)
+
+c //= a
+print ("Line 7 - Value of c is (c //=a)", c)
