53bcf285539228bf4f9a4b921343c90dd2de9ac3
vijay patel <to.patelvijay@gmail.com>
2019-10-22 11:23:35 +0530

    added python class example

---
+++ Python-with-class/Class_with_attribute.py
@@ -0,0 +1,23 @@
+
+#################################################
+# Python Class with Attribute Example
+#################################################
+
+class class_with_Attribute:
+#class attribute Declaration
+        type="Normal"
+
+t1 = class_with_Attribute()
+t2 = class_with_Attribute()
+
+#Getting Output of Class Attribute
+print (t1.type,t2.type)
+#Output would be
+# Normal Normal
+
+#updating class Attribute value
+testing.type="Advance"
+
+print (t1.type,t2.type)
+#Output would be
+# Advance Advance
